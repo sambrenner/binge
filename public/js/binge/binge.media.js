02b5aa5daf8f157a0f2184926a060045b9e79b0e
Sam Brenner <samjbrenner@gmail.com>
2014-02-10 17:26:31 -0500

    fixing selectors for transcript highlighting

--- public/js/binge/binge.media.js
+++ public/js/binge/binge.media.js
@@ -12,7 +12,7 @@ var binge = (function(window, document, parent) {
   parent.media.initPopcorn = function() {
     _popcorn = Popcorn("#binge_audio");
 
-    parent.$container.find('.binge-transcript').each(function() {
+    parent.$transcript.find('section').each(function() {
       var $this = $(this);
 
       _popcorn.code({
